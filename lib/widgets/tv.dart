89b330943ea6e880a15feecc4591b1b060345392
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-20 16:42:29 +0530

    Top Banner with Carousel Slider

--- lib/widgets/tv.dart
+++ lib/widgets/tv.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/utils/text.dart';
 import 'package:tmdb_api/tmdb_api.dart';
@@ -23,71 +24,72 @@ class TV extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
-          Container(
-            height: 200,
-            child: ListView.builder(
-                itemCount: tv.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Description(
-                                    name: tv[index]['original_name'].toString(),
-                                    bannerurl:
-                                        'https://image.tmdb.org/t/p/w500' +
-                                            tv[index]['backdrop_path'],
-                                    posterurl:
-                                        'https://image.tmdb.org/t/p/w500' +
-                                            tv[index]['poster_path'],
-                                    description: tv[index]['overview'],
-                                    vote: tv[index]['vote_average'].toString(),
-                                    launch_on:
-                                        tv[index]['first_air_date'].toString(),
-                                  )));
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      width: 250,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            tv[index]['original_name'] != null
-                                ? Container(
-                                    width: 250,
-                                    height: 140,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                      image: DecorationImage(
-                                        image: NetworkImage(
-                                          'https://image.tmdb.org/t/p/w500' +
-                                              tv[index]['backdrop_path'],
-                                        ),
-                                        fit: BoxFit.cover,
-                                      ),
+          CarouselSlider(
+            items: List.generate(tv.length, (index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Description(
+                                name: tv[index]['original_name'].toString(),
+                                bannerurl: 'https://image.tmdb.org/t/p/w500' +
+                                    tv[index]['backdrop_path'],
+                                posterurl: 'https://image.tmdb.org/t/p/w500' +
+                                    tv[index]['poster_path'],
+                                description: tv[index]['overview'],
+                                vote: tv[index]['vote_average'].toString(),
+                                launch_on:
+                                    tv[index]['first_air_date'].toString(),
+                              )));
+                },
+                child: Container(
+                  width: 280,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        tv[index]['original_name'] != null
+                            ? Container(
+                                width: 280,
+                                height: 160,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      'https://image.tmdb.org/t/p/w500' +
+                                          tv[index]['backdrop_path'],
                                     ),
-                                  )
-                                : Container(),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              child: modified_text(
-                                text: tv[index]['original_name'] != null
-                                    ? tv[index]['original_name']
-                                    : 'Loading',
-                                color: Colors.white,
-                                size: 16,
-                              ),
-                            )
-                          ],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              )
+                            : Container(),
+                        SizedBox(
+                          height: 10,
                         ),
-                      ),
+                        Container(
+                          child: modified_text(
+                            text: tv[index]['original_name'] != null
+                                ? tv[index]['original_name']
+                                : 'Loading',
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                        )
+                      ],
                     ),
-                  );
-                }),
+                  ),
+                ),
+              );
+            }),
+            options: CarouselOptions(
+              autoPlay: true,
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+            ),
           ),
         ],
       ),
