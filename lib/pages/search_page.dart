4b2efcfe4e6afa7baf0968db6e39a66c7e2c257f
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-21 00:53:18 +0530

    Stepping Towards Search Page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:movie_app/widgets/searching.dart';
 import 'package:tmdb_api/tmdb_api.dart';
+
+import '../utils/text.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -29,7 +32,6 @@ class _SearchState extends State<SearchPage> {
 
     setState(() {
       searchmovies = searchresult['results'];
-      // loadsearchmovies = loadsearchresult['results'];
     });
     print(searchresult);
   }
@@ -38,42 +40,61 @@ class _SearchState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      // appBar: AppBar(
-      //   title: const Text('Search Page'),
-      // ),
-      body: SingleChildScrollView(
-          child: SafeArea(
-        child: Column(children: [
-          Container(
-            height: 60,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              color: Color(0xFF292B37),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(children: [
-              Icon(Icons.search, color: Colors.white54, size: 30),
-              Container(
-                width: 300,
-                margin: EdgeInsets.only(left: 5),
-                child: TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Search",
-                      hintStyle: TextStyle(color: Colors.white54)),
-                ),
-              ),
-              SizedBox(height: 30),
-              // UpcomingWidget(),
-            ]),
-          )
-        ]),
-      )),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: modified_text(
+          text: 'Search Page',
+          color: Colors.yellow,
+          size: 20,
+        ),
+      ),
+      body: ListView(
+        children: [Searching(searching: searchmovies)],
+      ),
     );
   }
 }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Colors.black,
+  //     // appBar: AppBar(
+  //     //   title: const Text('Search Page'),
+  //     // ),
+  //     body: SingleChildScrollView(
+  //         child: SafeArea(
+  //       child: Column(children: [
+  //         Container(
+  //           height: 60,
+  //           padding: EdgeInsets.all(10),
+  //           margin: EdgeInsets.symmetric(horizontal: 10),
+  //           decoration: BoxDecoration(
+  //             color: Color(0xFF292B37),
+  //             borderRadius: BorderRadius.circular(10),
+  //           ),
+  //           child: Row(children: [
+  //             Icon(Icons.search, color: Colors.white54, size: 30),
+  //             Container(
+  //               width: 300,
+  //               margin: EdgeInsets.only(left: 5),
+  //               child: TextFormField(
+  //                 style: TextStyle(color: Colors.white),
+  //                 decoration: InputDecoration(
+  //                     border: InputBorder.none,
+  //                     hintText: "Search",
+  //                     hintStyle: TextStyle(color: Colors.white54)),
+  //               ),
+  //             ),
+  //             SizedBox(height: 30),
+  //             // UpcomingWidget(),
+  //           ]),
+  //         )
+  //       ]),
+  //     )),
+  //   );
+  // }
+
 
   // @override
   // _SearchState createState() => _SearchState();
---
+++ lib/widgets/searching.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/utils/text.dart';
+
+class Searching extends StatelessWidget {
+  const Searching({super.key, required this.searching});
+  final List searching;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
